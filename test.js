ac91281a6f4e9a01276e3e3f6a4cc15aec588a2b
kilkat <rhkddns3459@naver.com>
2023-03-14 21:50:23 +0900

    2023-03-14_kilkat

--- test.js
+++ test.js
@@ -1,44 +1,56 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-// 웹 크롤링 함수
-async function crawl(url, depth) {
+// { link: string, isExplored: boolean }[]
+let links_arr = [];
+
+async function crawl(url, tried = 0) {
   try {
-    if (depth < 0) {
-      return;
-    }
-
-    // 웹 페이지의 HTML 코드 가져오기
     const response = await axios.get(url);
     const html = response.data;
 
-    // HTML 코드 파싱하기
     const $ = cheerio.load(html);
 
-    // 웹 페이지에서 href 속성이 있는 모든 a 태그 추출하기
     const links = $('a[href^="/"]');
 
-    // a 태그 출력하기
-    console.log('===', url, '===');
     links.each((index, element) => {
-      console.log($(element).attr('href'));
+      const current = $(element).attr("href");
+      if (!links_arr.some((item) => item.link === current)) {
+        links_arr.push({ link: current, isExplored: false });
+      }
     });
-    console.log();
 
-    // 재귀적으로 웹 페이지 크롤링하기
-    for (let i = 0; i < links.length; i++) {
-      const link = $(links[i]);
-      const href = link.attr('href');
-
-      // 상대 경로를 절대 경로로 변환하기
-      const absoluteUrl = new URL(href, url).href;
-
-      await crawl(absoluteUrl, depth - 1);
+    if (!links_arr.some((item) => !item.isExplored)) {
+      return links_arr;
     }
+
+    const crawlPromises = links_arr.map(async (item, index) => {
+      if (!item.isExplored) {
+        links_arr[index].isExplored = true;
+        const result = await crawl(url + item.link, tried++);
+        return result;
+      }
+    });
+
+    const results = await Promise.all(crawlPromises);
+    const flattenedResults = results.flat();
+
+    // Filter out duplicate links
+    const uniqueLinksArr = flattenedResults.filter((item, index) => {
+      return (
+        index === flattenedResults.findIndex((obj) => obj.link === item.link)
+      );
+    });
+
+    return uniqueLinksArr;
   } catch (error) {
     console.error(error);
   }
 }
 
-// 크롤링 시작하기
-crawl('http://cuha.cju.ac.kr', 10);
+const init = async () => {
+  const res = await crawl("http://cuha.cju.ac.kr");
+  console.log(res);
+};
+
+init();
