c0f6ed1cb4c88d5ae917b9d53af428dc06912063
KILKAT_NOTEBOOK <rhkddns3459@naver.com>
2023-03-16 13:38:57 +0900

    2023-03-16_kilkat

--- router/index.js
+++ router/index.js
@@ -7,4 +7,15 @@ router.get("/", (req, res) => {
     res.render("index.ejs");
 })
 
+router.get("/login", (req, res) => {
+    res.render("login.ejs");
+})
+
+router.get("/register", (req, res) => {
+    res.render("register.ejs");
+})
+
+router.post("/site-scaning", controller.site-scaning);
+
+
 module.exports = router;
