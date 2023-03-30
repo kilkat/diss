afcb70b35107178e769304504e0df6db5c779bfd
KILKAT_NOTEBOOK <rhkddns3459@naver.com>
2023-03-30 13:09:43 +0900

    2023-03-30_kilkat

--- router/index.js
+++ router/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 
 const router = express.Router();
 
+const attack_controller = require('../controller/attack')
+
 router.get("/", (req, res) => {
     res.render("index.ejs");
 })
@@ -14,6 +16,8 @@ router.get("/login", (req, res) => {
 router.get("/register", (req, res) => {
     res.render("register.ejs");
 })
+
+router.get("/reflected-xss-success", attack_controller.reflected_xss_success);
 
 router.post("/site-scaning", controller.site-scaning);
 
