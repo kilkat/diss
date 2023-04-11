f031b959e4a1205540d25f8d36eece4d4911293c
CyberSecurityHat <godofwhiteworld@gmail.com>
2023-04-11 14:11:58 +0900

    2023-04-11_CyberSecurityHat

--- models/index.js
+++ models/index.js
@@ -6,7 +6,8 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../config/config.js`)[env];
+const config = require(__dirname + '/../config/config.json')[env];
+const User  = require('./users');
 const db = {};
 
 let sequelize;
@@ -16,28 +17,12 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-// fs
-//   .readdirSync(__dirname)
-//   .filter(file => {
-//     return (
-//       file.indexOf('.') !== 0 &&
-//       file !== basename &&
-//       file.slice(-3) === '.js' &&
-//       file.indexOf('.test.js') === -1
-//     );
-//   })
-//   .forEach(file => {
-//     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-//     db[model.name] = model;
-//   });
-
-// Object.keys(db).forEach(modelName => {
-//   if (db[modelName].associate) {
-//     db[modelName].associate(db);
-//   }
-// });
-
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
+db.User = User;
+User.init(sequelize);
+
+User.associate(db);
 
 module.exports = db;
