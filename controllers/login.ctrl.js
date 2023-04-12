42fa8f9aed92edd3ba95135978fdf55ead859ba0
CyberSecurityHat <godofwhiteworld@gmail.com>
2023-04-12 15:58:34 +0900

    2023_04_12CyberSecurityHat

--- controllers/login.ctrl.js
+++ controllers/login.ctrl.js
@@ -2,6 +2,7 @@ const router = require("express");
 const mysql = require("mysql2");
 const User = require("../models/users");
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 const emailExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i; //email regExp
 const passwordExp = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,18}$/; //password regExp
@@ -22,22 +23,26 @@ const loginUser = async(req, res) => {
     try{
         
         const exUser = await User.findOne({where: {email: email}});
+        if(!exUser) {
+            return res.send("<script>alert('로그인에 실패했습니다.');location.href='/login';</script>");
+        }
+
         const pass = await bcrypt.compare(password, exUser.password);
 
         if(pass) {
-            if(exUser !== null){
-                req.session.email = email;
-                req.session.is_logined = true;
-                req.session.save(function() {
-                });
-                console.log("testtest")
-                console.log(email)
-                console.log(password)
-                return res.send("<script>alert('로그인에 성공했습니다.');location.href='/';</script>");
-            }else{
-                return res.send("<script>alert('로그인에 실패했습니다.');location.href='/login';</script>");
-            }
-        } else {
+            req.session.email = email;
+            req.session.is_logined = true;
+            req.session.save(function() {
+            });
+            
+            // JWT 토큰 발급
+            const token = jwt.sign({ userNumber: exUser.number }, 'my_secret_key');
+            
+            // 토큰을 클라이언트에게 보냄
+            // res.json({ token });
+
+            return res.send("<script>alert('로그인에 성공했습니다.');location.href='/';</script>" + {token});
+        }else{
             return res.send("<script>alert('로그인에 실패했습니다.');location.href='/login';</script>");
         }
     }catch(err){
--- router/index.js
+++ router/index.js
@@ -1,6 +1,7 @@
-const { Router } = require("express");
 const express = require("express");
 const path = require("path");
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
@@ -28,6 +29,29 @@ router.get("/register", (req, res) => {
 router.post("/register", registerController.createUser);
 
 router.get("/reflected-xss-success", attackController.reflected_xss_success); //url에 세션 아이디 넘겨줘야됨
+
+router.get('/protected', (req, res) => {
+    // 헤더에서 토큰을 가져옴
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).json({ message: 'Authorization header missing' });
+    }
+  
+    const token = authHeader.split(' ')[1];
+  
+    // 토큰 검증
+    jwt.verify(token, 'my_secret_key', (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: 'Invalid token' });
+      }
+  
+      // 요청한 사용자 ID를 가져와서 사용자 데이터를 찾음
+      const userNumber = decoded.userNumber;
+      const user = users.find(u => u.number === userNumber);
+  
+      res.send({ message: `Hello, ${user.name}!` });
+    });
+  });
 
 // router.post("/site-scaning", controller.site-scaning);
 
