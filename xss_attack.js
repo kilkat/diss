32bc914acac4c59f06b483855fcbda6a1fe34f63
KILKAT_NOTEBOOK <rhkddns3459@naver.com>
2023-04-13 16:37:14 +0900

    2023-04-13_kilkat

--- xss_attack.js
+++ xss_attack.js
@@ -5,5 +5,10 @@ const url = 'http://cuha.cju.ac.kr';
 const array = fs.readFileSync('payload.txt').toString().split("\n");
 
 for(i in array) {
-    console.log( i + '번째 payload 입니다 : ' + array[i]);
+    // console.log( i + '번째 payload 입니다 : ' + array[i]);
+    let victim_url = url + array[i];
+    console.log(victim_url);
+
+    //url 받고 query 날리는 로직 짜면됨
+
 }
