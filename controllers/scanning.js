14ed133cbe3d898537e169f35b22d728f4b92b14
CyberSecurityHat <godofwhiteworld@gmail.com>
2023-04-27 19:48:05 +0900

    2023-04-27_CyberSecurityHat

--- controllers/scanning.js
+++ controllers/scanning.js
@@ -11,15 +11,25 @@ const scanning = async(req, res) => {
     try{
 
         for (let i in array) {
-            let victim_url = url + "?" + array[i];
+            let victim_url = url + "?" + "firstname=" + array[i] + "&" + "lastname=random";
             console.log(victim_url);
             const options = {
                 url: victim_url,
+                headers: {
+                    'Cookie': "b4126d5f746bd84bc67f590706290dee",
+                    'security_level': "0"
+                },
             };
             request.post(options, function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    console.log(body);
-                }
+                if (error) {
+                    console.error('에러:', error);
+                  } else if (response.statusCode !== 200) {
+                    console.error('HTTP 요청 에러:', response.statusCode, body);
+                  } else {
+                    console.log('요청 성공:', body);
+                    console.log(victim_url);
+                    console.log('----------------------------------------------');
+                  }
             });
         }
 
