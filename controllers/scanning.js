5ee3a86a5da4726c9bc9bc09615219e906f777dc
CyberSecurityHat <godofwhiteworld@gmail.com>
2023-04-28 19:45:15 +0900

    2023-04-28_CyberSecurityHat

--- controllers/scanning.js
+++ controllers/scanning.js
@@ -1,35 +1,46 @@
 const fs = require('fs');
 const readline = require('readline');
 const open = require('open');
+const { chromium } = require('playwright');
 
 const array = fs.readFileSync('payload.txt').toString().split("\n");
 
 const scanning = async(req, res) => {
-
     const url = req.body.href;
-
-    try{
+    const timeout = 30000;
+    
+    try {
+        // 브라우저 인스턴스 생성
+        const browser = await chromium.launch();
+        
+        // 새로운 Context 인스턴스 생성
+        const context = await browser.newContext();
+        
+        // 새로운 Page 인스턴스 생성
+        const page = await context.newPage();
+        
+        // 페이지 로딩 완료시까지 대기
+        page.on('load', () => console.log('Page loaded!'));
+        page.on('error', err => console.log(`Error occured: ${err}`));
 
         for (let i in array) {
             let victim_url = url + array[i];
             console.log(victim_url);
-            const browser = await open(victim_url)
-                .then(browser => {
-                    // 5초 후 브라우저 종료
-                    setTimeout(() => {
-                    browser.kill();
-                    console.log(`Closed ${url} in ${browser.pid}`);
-                    }, 3000);
-                })
-                .catch(error => {
-                    console.error(error);
-                });
-            console.log(i);
+
+            // 페이지 이동
+            await page.goto(victim_url, {timeout: timeout, waitUntil: 'domcontentloaded'});
+
+            // 일정 시간 후 브라우저 종료
+            await page.waitForTimeout(5000);
         }
-    }catch(error){
-        console.log(error);
+
+        // 브라우저 종료
+        await browser.close();
+        console.log('Browser closed!');
+    } catch (error) {
+        console.error(error);
     }
-}
+};
 
 const count = async(req, res) => {
     let cnt = 0;
