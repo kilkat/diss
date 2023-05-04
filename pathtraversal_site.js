2f983639153f6cfe124c1facea8372f46250868e
KILKAT_NOTEBOOK <rhkddns3459@naver.com>
2023-05-04 15:02:25 +0900

    2023-05-04_kilkat

--- pathtraversal_site.js
+++ pathtraversal_site.js
@@ -3,8 +3,9 @@ const fs = require('fs');
 const app = express();
 
 app.get('/download', (req, res) => {
-  const fileName = req.query.file;
-  const filePath = `${__dirname}/uploads/${fileName}`;
+  const fileName = req.query.file; // /download 경로에 query.file /download?file= 형식으로 받아오는중
+  //입력값에 대한 검증이 없음 -> pathtraversal 취약점 발생
+  const filePath = `${__dirname}/${fileName}`;
   fs.readFile(filePath, (err, data) => {
     if (err) {
       res.status(500).send('Error');
