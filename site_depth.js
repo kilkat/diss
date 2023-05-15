8af13c535088e2d228171107a74b293a5a16c0bf
CyberSecurityHat <godofwhiteworld@gmail.com>
2023-05-15 21:48:01 +0900

    2023-05-15_CyberSecurityHat

--- site_depth.js
+++ site_depth.js
@@ -1,76 +1,45 @@
-const puppeteer = require('puppeteer');
+const axios = require('axios');
 const cheerio = require('cheerio');
-const { response } = require('express');
 
-const url = 'http://cuha.cju.ac.kr'
+// 크롤링할 웹사이트의 시작 URL
+const startUrl = 'http://localhost';
 
-const site_scan = async(req, res) => {
-  try{
+// 방문한 URL을 저장할 Set 객체
+const visitedUrls = new Set();
 
-    const max_depth = 10;
-    let list = [];
-    let sub_list = [];
-    const merged = list.concat(sub_list);
-    let unique_list = merged.filter((item, pos) => merged.indexOf(item) === pos);
-  
-    const browser = await puppeteer.launch({headless:'new'});
-    const page = await browser.newPage();
-  
-    await page.goto(url);
-  
-    const html = await page.content();
+// 깊이 우선 탐색(Depth First Search) 함수
+async function crawl(url) {
+  try {
+    // 이미 방문한 URL이라면 종료
+    if (visitedUrls.has(url)) {
+      return;
+    }
+
+    // URL 방문 체크
+    visitedUrls.add(url);
+
+    // HTTP GET 요청으로 HTML 문서 가져오기
+    const response = await axios.get(url);
+    const html = response.data;
+
+    // Cheerio를 사용하여 HTML 문서를 파싱
     const $ = cheerio.load(html);
 
+    // 원하는 정보를 추출하거나 다음 링크를 찾는 작업 수행
+
+    // 예시: 다음 링크 탐색
     $('a').each((index, element) => {
       const href = $(element).attr('href');
-      if (href) {
-        list.push(href);
-      }else{
-        console.log("href가 존재하지 않습니다.");
+      if (href && href.startsWith('/')) {
+        const absoluteUrl = new URL(href, url).href;
+        console.log(absoluteUrl);
+        crawl(absoluteUrl);
       }
     });
-
-    list.each(list, function(index, element){
-      for(const i = 0; i < max_depth; i++){
-
-        const browser = puppeteer.launch({headless:'new'});
-        const page = browser.newPage();
-
-        const sub_url = url + element + sub_list[i];
-
-        if(!(sub_url.includes(sub_url))){
-          const request = page.goto(sub_url);
-          const status = request.status();
-      
-          if(status == 200){
-
-            const html = page.content();
-            const $ = cheerio.load(html);
-
-            $('a').each((index, element) => {
-              const href = $(element).attr('href');
-              if (href) {
-                sub_list.push(href);
-              }else{
-                console.log("href가 존재하지 않습니다.");
-              }
-            });
-
-          }else{
-            console.log('404 PAGE NOT FOUND');
-          }
-
-        }
-
-      }
-    });
-
-    console.log("list = " + list);
-    console.log("sub_list = " + sub_list);
-    console.log("unique_list = " + unique_list);
-
-
-  }catch(error){
-    console.log(error);
+  } catch (error) {
+    console.error('Error:', error);
   }
 }
+
+// 크롤링 시작
+crawl(startUrl);
